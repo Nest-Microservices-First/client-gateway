218638e3e2f92284166fa2db584ffc3b7cfd1440
Gabriel Barbarin Gorostegui <gabriel.barbarin@clouddistrict.com>
2024-04-15 08:36:30 +0200

    Initial creation of the product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   constructor() {}
+
+  @Post()
+  createProduct() {
+    return 'Crea un producto';
+  }
+
+  @Get()
+  findProducts() {
+    return 'Busca listado de productos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return `Busca un producto con id: ${id}`;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return `Elimina un producto con id: ${id}`;
+  }
+
+  @Patch(':id')
+  updateProduct(@Body() body: any, @Param('id', ParseIntPipe) id: number) {
+    return `Edita un producto con id: ${id}`;
+  }
 }
